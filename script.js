671ca2027ec757f3dbdef6bd35ba0d6653fc6df7
LucasH <lucas.huygen@student.ehb.be>
2024-06-03 21:35:20 +0200

    ervoor zorgen als je refresh dat het blijft staan

--- script.js
+++ script.js
@@ -3,24 +3,46 @@
 //element selecteren
 //button selecteren
 const btnToevoegen = document.getElementById('btnToevoegen');
+const iptOpdracht = document.querySelector('#iptOpdracht');
+const ToDoLijst = document.getElementById('ToDoLijst');
 
+// Event listener voor de toevoeg knop
+btnToevoegen.addEventListener('click', function(event) {
+    event.preventDefault();
+    frmValideren();
+});
+
+ToDoLijst.innerHTML = 
+         `<div class="ToDoBox">
+         <p>${localStorage.getItem("To-Do")}</p>
+         <p>${localStorage.key("To-Do")}</p>
+         <button class="verwijderen">Verwijder</button>
+         </div>`
 })();
 
 function frmValideren() {
-    let iptOpdracht = document.forms["frmToevoegenToDo"]["iptOpdracht"].value;
+   
     //checken of de input leeg is
-    if (iptOpdracht == '') {
-        window.alert('Je moet iets invullen' + iptOpdracht);
+    if (iptOpdracht.value == '') {
+        window.alert('Je moet iets invullen' + iptOpdracht.value);
         return false;
     }
     else {
-        window.alert('Opdracht is toegevoegd!' + iptOpdracht)
+        window.alert('Opdracht is toegevoegd!' + iptOpdracht.value)
 
          //value in localstorage zetten
-         localStorage.setItem("To-Do", iptOpdracht);
+         localStorage.setItem("To-Do", iptOpdracht.value);
 
-         document.getElementById("test").innerHTML = localStorage.getItem("To-Do");
+         //div aanmaken en todo boodschap erin zetten samen met verwijderen knop
+         ToDoLijst.innerHTML = 
+         `<div class="ToDoBox">
+         <p>${localStorage.getItem("To-Do")}</p>
+         <p>${localStorage.key("To-Do")}</p>
+         <button class="verwijderen">Verwijder</button>
+         </div>`
+         //document.getElementById("ToDoLijst").innerHTML = localStorage.getItem("To-Do");
 
+        
         return false;
     }
 }
